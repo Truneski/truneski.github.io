3fb0d89a7d832799b88af49cf12fd72c33fad3fd
Trunesec <vincetrune@gmail.com>
2016-12-21 10:03:12 +0100

    Updating Blog

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,18 @@
 # @truneski -2016
 
 set -e
+# Build the project.
+echo -e "\033[0;32mBuilding the project...\033[0m"
+cd /home/ubuntu/my-website 
+hugo
+echo -e "\033[0;32mUpdating Blog Source to Github...\033[0m"
+git add -A
+git commit -m 'Updating Blog Source'
+git push origin master
+hugo -d /home/ubuntu/truneski.github.io/
 
 # Update local source with remote
+cd /home/ubuntu/truneski.github.io
 echo -e "\033[0;32mUpdating local copy of project master..\033[0m"
 git pull origin master
 
@@ -17,6 +27,4 @@ git commit -m 'Updating Blog'
 echo -e "\033[0;32mDeploying source to Github...\033[0m"
 git push origin master
 
-# Build the project.
-echo -e "\033[0;32mBuilding the project...\033[0m"
-hugo
+
